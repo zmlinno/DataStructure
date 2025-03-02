6a70c963b047dbdf30bbdad5f0a9a777f2e7d205
zmlinno <131747526+zmlinno@users.noreply.github.com>
2025-03-03 01:06:44 +0900

    Update SingleLinkedList.h

--- LinkedList/SingleLinkedList.h
+++ LinkedList/SingleLinkedList.h
@@ -310,6 +310,61 @@ ListNode* reverseKGroup(ListNode* head, int k) {
 
     return dummy.next;
 }
+
+
+
+//力扣148 排序链表-中等
+//找到链表中的中点如快慢指针
+ListNode* getMid(ListNode*head)
+{
+    ListNode* slow = head,*fast = head,*prev = nullptr;
+    while(fast&&fast->next)
+    {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if(prev)prev->next = nullptr;//断开链表
+    return slow;
+}
+
+//合并两个有序链表
+ListNode* merge(ListNode*h1,ListNode*h2)
+{
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+    while(h1&&h2)
+    {
+        if(h1->val < h2->val)
+        {
+            tail->next = h1;
+            h1 = h1->next;
+        }
+        else
+        {
+            tail->next = h2;
+            h2 = h2->next;
+        }
+        tail = tail->next;
+    }
+    tail->next = h1?h1:h2;
+    return dummy.next;
+}
+
+//归并排序链表
+ListNode* sortList(ListNode* head)
+{
+    if(!head||!head->next)return head;
+    ListNode* mid = getMid(head);
+    ListNode* left = sortList(head);
+    ListNode* right = sortList(mid);
+    return merge(left,right);
+}
+
+
+
+
+
 //创建链表
 // 创建链表
 ListNode* createList(initializer_list<int> vals) {
